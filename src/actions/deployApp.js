f5764916f39100decb270963bbb7c8687dbc6d2b
Peter Kota <kotapeter@gmail.com>
2025-06-05 08:47:36 +0200

    feat: use deploymentId in pollStatus

--- src/actions/deployApp.js
+++ src/actions/deployApp.js
@@ -39,17 +39,17 @@ const initDeployment = async () => {
 }
 
 const maxRetries = 1000
-const pollStatus = async (retry = 0) => {
+const pollStatus = async (deploymentId, retry = 0) => {
   await new Promise((res) => setTimeout(res, 5000))
   if (retry >= maxRetries) {
     throw new Error('Max retries reached while polling deployment status')
   }
-
+  const sevallaToken = core.getInput('sevalla-token')
   const resp = await fetch(`https://api.sevalla.com/v2/deployments/${deploymentId}`, {
     headers: { Authorization: `Bearer ${sevallaToken}` },
   })
   if (!resp.ok) {
-    return pollStatus(retry + 1)
+    return pollStatus(deploymentId, retry + 1)
   }
   const data = await resp.json()
   const status = data?.status
@@ -60,7 +60,7 @@ const pollStatus = async (retry = 0) => {
       throw new Error(`Deployment failed: status is ${status}`)
     }
   } else {
-    return pollStatus(retry + 1)
+    return pollStatus(deploymentId, retry + 1)
   }
 }
 
